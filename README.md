39b9bc12ed1ec6166f02ab36142827be730d7fe5
dickson4954 <dicksonmurithi155@gmail.com>
2024-04-12 13:48:19 +0300

    complete

---
+++ README.md
@@ -0,0 +1,28 @@
+#### weather api project 
+
+##### Date,2024/04/11
+
+##### By *Dickson Murithi*
+
+
+#### DESCRIPTION
+Use open weather map API go to open weathermap website
+https://openweathermap.org/
+sign for a free account and log in and then you navigate to the api keys section and you generate a new key and copy it and inside your javascripts you change it with the constant  YOUR_API_KEY.
+
+### CONST API_KEY="YOUR_API_KEY"
+
+##### creating a weather API PROJECT
+
+#### git
+
+
+#### 
+
+#### [git]{gh-pages link}
+
+#### Html,Css,javascript,Github
+
+##### github.com/dickson4954
+
+##### The content of the site is licensed under the mit license copyright(c) 2024.
---
+++ script.js
@@ -0,0 +1,84 @@
+
+const API_KEY="5f0114f38850310d50fce18127c91b5e";//api key for open weather Api
+const cityInput=document.querySelector(".city-input");
+const searchButton=document.querySelector(".search-btn")
+const weathercardsDiv=document.querySelector(".Current-weather")
+const currentweatherDiv=document.querySelector(".weather-cards")
+
+
+
+const createweathercard= (cityName,weatherItem, index)=>{
+   if(index === 0){//html for the main weather card
+     return ` <div class="details">
+         <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+         <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+         <h4>Wind:${weatherItem.wind.speed}M/S</h4>
+         <h4>Humidity:${weatherItem.main.humidity}%</h4>
+         
+    </div>
+    <div class="icon">
+         <img src= "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+         <h4>${weatherItem.weather[0].description}</h4>
+    </div>`;
+   }else{ //html for the other five day forecast card
+    return `<li class="card">
+                <h2>(${weatherItem.dt_txt.split(" ")[0]})</h2>
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+                <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                <h4>Wind:${weatherItem.wind.speed}M/S</h4>
+                <h4>Humidity:${weatherItem.main.humidity}%</h4>
+            </li>`;
+   }
+}
+const getWeatherDetails=(cityName,lat,lon)=>{
+    const WEATHER_API_URL=`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+
+    fetch(WEATHER_API_URL).then(res=>res.json()).then(data=>{
+        
+        //filter the forecasts to get only one forecast per day
+        const uniqueForcastDays=[];
+        const fiveDaysForecast= data.list.filter(forecast=> {
+        const forecastDate=new Date(forecast.dt_txt).getDate();
+        if(!uniqueForcastDays.includes(forecastDate)){
+            return uniqueForcastDays.push(forecastDate);
+        }
+        });
+
+        //clearing the previous weather data
+        cityInput.value="";
+        weathercardsDiv.innerHTML="";
+        currentweatherDiv.innerHTML="";
+
+        //creating weather cards and adding them to the Dom
+        fiveDaysForecast.forEach((weatheritem,index )=> {
+
+        if(index===0){
+            weathercardsDiv.insertAdjacentHTML("beforeend",createweathercard(cityName,weatheritem,index));
+        }else{
+            weathercardsDiv.insertAdjacentHTML("beforeend",createweathercard(cityName,weatheritem,index));
+        }
+            
+             });
+    })  .catch(()=>{
+            alert("An error occured while fetching the weather forecast!")
+        })
+    
+}
+
+const getcitycoordinates=()=>{
+    const cityName=cityInput.value.trim();
+    if(!cityName)return;//return if city name is empty
+    const GEOCODING_API_URL=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
+
+    //get entered city coordinates(lattiude,longitude,and name)from api responses
+    fetch(GEOCODING_API_URL).then(res=>res.json()).then(data=>{
+        if(!data.length)return alert(`No coordinates found for${cityName}`);
+        const {name,lat,lon}=data[0]
+        getWeatherDetails(name,lat,lon);
+    }).catch(()=>{
+        alert("An error occured while fetching the coordinates")
+    })
+}
+
+searchButton.addEventListener("click",getcitycoordinates);
+
